f9210c9e1a68b6d1a06d0451639d59c4b3268977
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2026-09-21 17:44:55 +0500

    Refactor and uncomment button functionality

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 /* Button */
-// let increase_count = document.getElementById("increase-count")
-// let count = 0
-// function count_it() {
-//     count += 1
-//     increase_count.innerText = count
-// }
-
-// let saveEl = document.getElementById("save-el")
-// function save() {
-//     // let entries_txt = " " + count + " - "
-//     let entries_txt = " - " + count + " "
-//     saveEl.innerText += entries_txt
-// }
-
-// let zero = document.getElementById("increase-count")
-// function make_0 () {
-// zero.innerText = 0
-// count = 0
-// }
-
-
-
-
-
 let increase_count = document.getElementById("increase-count");
 let saveEl = document.getElementById("save-el");
 let count = 0;
